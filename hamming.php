b4fb3fcc900e11e07e8a5cc095e01cc8bf677109
Leagian <lea.giangrosso@gmail.com>
2023-02-23 15:54:18 +0100

    exo4

--- hamming.php
+++ hamming.php
@@ -1,5 +1,8 @@
 <?php
 function distance($strandA,$strandB) {
+    if (strlen($strandA) !== strlen($strandB)) {
+        throw new Error("Strings must be of the same length");
+      }
     $result = 0;
     for($i = 0; $i < strlen($strandA); $i++) {
         if($strandA[$i] !== $strandB[$i]) {
